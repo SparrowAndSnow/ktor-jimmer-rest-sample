e6649831c6e7f57f17231a538be09db864ab01b0
annual_morning <361460536@qq.com>
2025-04-08 18:30:54 +0800

    sample

--- sample/book-service/src/main/kotlin/com/book/Routing.kt
+++ sample/book-service/src/main/kotlin/com/book/Routing.kt
@@ -10,37 +10,28 @@ import io.ktor.server.resources.Resources
 import io.ktor.server.routing.*
 import org.babyfish.jimmer.sql.kt.ast.expression.*
 import com.eimsound.ktor.route.*
-import com.eimsound.util.ktor.defaultValue
-import com.eimsound.util.ktor.queryParameterExt
 import dev.hayden.KHealth
+import java.math.BigDecimal
 
 fun Application.configureRouting() {
     install(Resources)
     install(KHealth)
     routing {
-        create<BookInput>("/book/create") {
-            validator {
-                with(it) {
-                    name.notBlank { "姓名不能为空" }
-                    price.range(0.toBigDecimal()..100.toBigDecimal()) { range ->
-                        "价格必须在${range.start}和${range.endInclusive}之间"
-                    }
-                }
-            }
-            entity {
-                it.copy(name = it.name.uppercase())
-            }
-        }
-        api<Book, BookInput>("/book") {
+        api<Book>("/book") {
             filter {
-//                where(specification<BookSpec>())
+                where(specification<BookSpec>())
                 where(
-                    table.name `ilike?` call.queryParameterExt<String>("name").defaultValue(),
-                    `between?`(table::price),
+                    table.name `ilike?` param<String>("name"),
+
+                    table.price.`between?`(
+                        param<BigDecimal>("price", "ge"),
+                        param<BigDecimal>("price", "le")
+                    )
+//                    `between?`(table::price),
                 )
 
                 where += table.authors {
-                    firstName `ilike?` this@api.call.queryParameterExt<String>("firstName").defaultValue()
+                    firstName `ilike?` this@filter.param<String>("firstName")
                 }
                 orderBy(table.id.desc())
             }
@@ -61,14 +52,11 @@ fun Application.configureRouting() {
             }
             validator {
                 with(it) {
-                    name.notBlank { "姓名不能为空" }
-                    price.range(0.toBigDecimal()..100.toBigDecimal()) { range ->
+                    ::name.notNull { "姓名不能为空" }
+                    ::price.range(0.toBigDecimal()..100.toBigDecimal()) { range ->
                         "价格必须在${range.start}和${range.endInclusive}之间"
                     }
                 }
-            }
-            entity {
-                it.copy(name = it.name.uppercase())
             }
         }
     }.getAllRoutes().forEach { log.info("Route: $it") }
